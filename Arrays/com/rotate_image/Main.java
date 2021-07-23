3b63599ef6912f441e8244cefcdddcb12f053097
Bharanidharan2K <bharanikkab123@gmail.com>
2021-07-23 19:35:51 +0530

    Arrays - rotate_image - corrections

--- Arrays/com/rotate_image/Main.java
+++ Arrays/com/rotate_image/Main.java
@@ -24,8 +24,8 @@ public class Main {
 		for(int i =0; i<matrix.length; i++) {
 			for(int j=0; j<matrix.length/2; j++) {
 				int temp = matrix[i][j];
-				matrix[i][j] = matrix[i][matrix.length-1];
-				matrix[i][matrix.length-1] = temp;
+				matrix[i][j] = matrix[i][matrix.length-1-j];
+				matrix[i][matrix.length-1-j] = temp;
 			}
 		}
 		System.out.println("After: ");
