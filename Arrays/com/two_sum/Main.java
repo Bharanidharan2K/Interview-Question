77a927855a63aed03450f735f1a3632eba78952b
Bharanidharan2K <bharanikkab123@gmail.com>
2021-07-23 19:06:19 +0530

    Arrays - two_sum

---
+++ Arrays/com/two_sum/Main.java
@@ -0,0 +1,27 @@
+package com.two_sum;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int nums[] = {2,7,11,15};
+		int target = 9;
+		int result[] = twoSum(nums, target);
+		for(int i: result) {
+			System.out.print(i+" ");
+		}
+	}
+	public static int[] twoSum(int[] nums, int target) {
+		int result[] = new int[2];
+		for(int i=0; i<nums.length; i++) {
+			for(int j=i+1; j<nums.length; j++) {
+				if(nums[i] + nums[j] == target) {
+					result[0] = i;
+					result[1] = j;
+					break;
+				}
+			}
+		}
+		return result;
+	}
+
+}
