a5944ab833bec531b73f3ae426912ae13b7d3248
Bharanidharan2K <bharanikkab123@gmail.com>
2021-07-24 19:10:22 +0530

    String - implement_strStr using O(n)2 time complexity

---
+++ String/com/implement_strStr/Main.java
@@ -0,0 +1,25 @@
+package com.implement_strStr;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String haystack = "hello", needle = "ll";
+		System.out.println(strStr(haystack, needle));
+	}
+	public static int strStr(String haystack, String needle) {
+		if(needle.isEmpty()) return 0;
+		int m = haystack.length();
+		int n = needle.length();
+		if(m < n) return -1;
+		for(int i=0; i<m; i++) {
+			int j;
+			for(j=0; j<n; j++) {
+				if(haystack.charAt(i+j) != needle.charAt(j)) 
+					break;
+			}
+			if(j == n) return i;
+		}
+		return -1;
+	}
+
+}
