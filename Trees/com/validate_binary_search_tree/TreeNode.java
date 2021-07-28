7364b75aac8f29836e8e420230b94920f21628ea
Bharanidharan2K <bharanikkab123@gmail.com>
2021-07-28 15:23:28 +0530

    Tree - validate_binary_search_tree

--- Trees/com/validate_binary_search_tree/TreeNode.java
+++ Trees/com/validate_binary_search_tree/TreeNode.java
@@ -8,9 +8,4 @@ public class TreeNode {
 	public TreeNode(int val) {
 		this.val = val;
 	}
-	public TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
-	}
 }
